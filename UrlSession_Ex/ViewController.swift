ac85c26c8dee3bc7ad25abf68a9752b5ffa03673
Karim <KariimAhmed8@gmail.com>
2020-06-19 12:50:19 +0200

    #Get & post using url session

--- UrlSession_Ex/ViewController.swift
+++ UrlSession_Ex/ViewController.swift
@@ -15,8 +15,6 @@ class ViewController: UIViewController {
     
     //instantiate url
      let url = URL(string: "https://jsonplaceholder.typicode.com/users")
-     let postUrl = URL(string: "https://jsonplaceholder.typicode.com/users")
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,22 +51,21 @@ class ViewController: UIViewController {
     
     ///MARK:- Post Api
     @IBAction func postApiCall(_ sender: Any) {
-        
-        var request = URLRequest(url: postUrl!)
+        var request = URLRequest(url: url!)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "post"
 
-     
-        let newPost = DataModel(name: "Karim", city: "Cairo", age: 100)
-        let encodedBody = try! JSONEncoder().encode(newPost)
-        request.httpBody = encodedBody
+        let paramters = ["name":"karim","age":"125"]
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: paramters, options: []) else {return}
+        request.httpBody = httpBody
         
         let session = URLSession.shared
         session.dataTask(with: request) { (data, response, err) in
 
             if let data = data {
                 do {
-                    let json = try JSONDecoder().decode(DataModel.self, from: data)
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
                     print("***\(json)")
                 }catch {
                     print(error)
